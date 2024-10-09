be12defb34b19b1d241174d1a0f040dcb64b7f81
EpphremM <mandefroephrem48@gmail.com>
2024-10-09 10:35:05 -0400

    trial

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,12 +1,16 @@
-import { Telegraf } from "telegraf"; // Ensure you're using the correct import statement
+import { Telegraf } from "telegraf";
 
-const TOKEN = "7578510001:AAEAmMM6VeOEmCXZkr8k5T_BOD-2Wwih1zM"; // Replace with your actual bot token
+const TOKEN = "7578510001:AAEAmMM6VeOEmCXZkr8k5T_BOD-2Wwih1zM"; 
 const bot = new Telegraf(TOKEN);
+const web_link='https://t.me/telbingo_bot/WaseBingo';
 
-bot.start((ctx) => ctx.reply("Welcome, Ephrem"));
+bot.start((ctx) => ctx.reply("Welcome, Ephrem",{
+    reply_markup:{keyboard:[[{text:"Bingo",web_app:{url: web_link}}]],}
+})
+);
 
 bot.launch().then(() => {
     console.log("Bot is up and running!");
-}).catch(err => {
+}).catch(err => { 
     console.error("Error launching the bot:", err);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
-import Button from './assets/components/Buttons/Button';
-import { useState,useEffect } from 'react';
-const tele=window.Telegram.WebApp;
-function App() {
+function App(){
   return (
     <>
    <h1>Hello it is me</h1>
-    <Button/>   
 
     </>
   )

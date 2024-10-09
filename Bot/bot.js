2c11821f0c2dfdc49089552c21cf6808e0e7e127
EpphremM <mandefroephrem48@gmail.com>
2024-10-09 11:22:43 -0400

    intial fixing

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,12 +1,15 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, Markup } from "telegraf";
 
 const TOKEN = "7578510001:AAEAmMM6VeOEmCXZkr8k5T_BOD-2Wwih1zM"; 
 const bot = new Telegraf(TOKEN);
 const web_link='https://t.me/telbingo_bot/WaseBingo';
 
-bot.start((ctx) => ctx.reply("Welcome, Ephrem",{
-    reply_markup:{keyboard:[[{text:"Bingo",web_app:{url: web_link}}]],}
-})
+bot.start((ctx) => {
+    ctx.reply("Welcome, Ephrem",
+    Markup.inlineKeyboard([
+        Markup.button.url('Open Mini App', web_link),
+    ])
+)}
 );
 
 bot.launch().then(() => {
